bc5955704402c851568bb3504df0d6ec24c1c475
Scott Francis <scott.francis@shopify.com>
2014-04-30 13:53:19 +0000

    Fix makefile

--- ext/alloc_track/extconf.rb
+++ ext/alloc_track/extconf.rb
@@ -4,7 +4,7 @@ have_func('rb_tracepoint_enable')
 gc_event = have_const('RUBY_INTERNAL_EVENT_NEWOBJ')
 
 if gc_event
-  create_makefile('alloc_tracker/alloc_track')
+  create_makefile('alloc_track/alloc_track')
 else
   File.open('Makefile', 'w') do |f|
     f.puts "install:\n\t\n"
